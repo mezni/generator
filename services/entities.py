10f29daabbf2d3ebf0f4aa49d0b32ebf9ca59018
mezni <mamezni@gmail.com>
2024-12-23 15:26:55 +0000

    generator 2024-12-23

---
+++ services/entities.py
@@ -0,0 +1,18 @@
+from typing import List, Tuple, Optional
+from dataclasses import dataclass
+
+
+@dataclass
+class ServiceType:
+    service_type_id: int
+    network_technology: str
+    service_type: str
+    bearer_type: str
+    managing_node: List[str]
+    jitter_range: Tuple[int, int]
+    latency_range: Tuple[int, int]
+    packet_loss_range: Tuple[int, int]
+    call_setup_time_range: Tuple[int, int]
+    mos_range: Tuple[int, int]
+    volte_flag: Optional[bool] = None
+    vowifi_flag: Optional[bool] = None
