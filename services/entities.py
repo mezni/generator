50ff76ccad7e564456d6c89868717f71f395e315
mezni <mamezni@gmail.com>
2024-12-23 17:01:10 +0000

    generator 2024-12-23

--- services/entities.py
+++ services/entities.py
@@ -16,3 +16,11 @@ class ServiceType:
     mos_range: Tuple[int, int]
     volte_flag: Optional[bool] = None
     vowifi_flag: Optional[bool] = None
+
+
+@dataclass
+class NetworkElementType:
+    network_element_type_id: int
+    network_technology: str
+    network_element_name: str
+    network_element_desc: str
